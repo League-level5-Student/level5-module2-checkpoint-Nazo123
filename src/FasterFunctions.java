ec885ba52854ed6fc7862ec4d25f809040b0da4f
Nazo123 <48037675+Nazo123@users.noreply.github.com>
2021-04-15 18:01:52 -0700

    Done

--- src/FasterFunctions.java
+++ src/FasterFunctions.java
@@ -1,12 +1,30 @@
+import java.util.Random;
 
 public class FasterFunctions {
 	public static void main(String[] args) {
 		float totalTime = 0;
 		long startTime = System.currentTimeMillis();
 		//Your Code Here
-		
-		
-		
+		Thread slowSortArray = new Thread(()-> {
+			SlowFunctions.slowSortLargeArray();
+		});
+		Thread ackerman = new Thread(()->{
+			SlowFunctions.ackermann(3, 10);
+		});
+		Thread sqrt = new Thread(()->{
+			SlowFunctions.millionsOfSqrts();
+		});
+		slowSortArray.start();
+		ackerman.start();
+		sqrt.start();
+		try {
+			slowSortArray.join();
+			ackerman.join();
+			sqrt.join();
+		}
+		catch(Exception e){
+			System.out.println("YOUR PROGRAM IS BROKEN");
+		}
 		
 		float endTime = (float)((double)System.currentTimeMillis() - (double)startTime) / 1000.0f;
 		totalTime += endTime;
